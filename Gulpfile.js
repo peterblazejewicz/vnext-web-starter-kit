efd7cf7510d94d0c7ed27fe772776436c4487b87
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2014-12-25 21:07:26 +0100

    Add Gulp build file to project

---
+++ Gulpfile.js
@@ -0,0 +1,22 @@
+'use strict'
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+var AUTOPREFIXER_BROWSERS = [
+  'ie >= 10',
+  'ie_mob >= 10',
+  'ff >= 30',
+  'chrome >= 34',
+  'safari >= 7',
+  'opera >= 23',
+  'ios >= 7',
+  'android >= 4.4',
+  'bb >= 10'
+];
+
+gulp.task('default', [], function(cb) {
+  console.log('Default task run');
+});
