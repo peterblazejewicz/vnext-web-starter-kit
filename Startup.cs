27512236d6973751521572909cee56e51fbc9e05
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-09-28 22:21:35 +0200

    Update project dependencies and assets [WIP]

--- Startup.cs
+++ Startup.cs
@@ -1,14 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
 
-namespace KWebStartup
+namespace EmptyApplication
 {
     public class Startup
     {
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
 
         public void Configure(IApplicationBuilder app)
         {
-            app.UseStaticFiles();
-            app.UseWelcomePage();
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
         }
     }
 }
